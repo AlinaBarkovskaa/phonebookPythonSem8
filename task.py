3579b99c395996ebca5c6c6ca63ac6ba88a077b5
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2023-02-22 23:35:54 +0400

    first commit

---
+++ task.py
@@ -0,0 +1,89 @@
+# Создать телефонный справочник с возможностью импорта и экспорта данных в формате .txt. 
+# Фамилия, имя, отчество, номер телефона - данные, которые должны находиться в файле.
+# Программа должна выводить данные
+# Программа должна сохранять данные в текстовом файле
+# Пользователь может ввести одну из характеристик для поиска определенной записи(Например имя или фамилию человека)
+# Использование функций. Ваша программа не должна быть линейной
+# 1. Вывод всех контактов
+# 2. Поиск контакта
+# 3. Добавить контакт (сразу сохрорнять в файл)
+# 4. Выход по требованию пользователя
+
+print('1 - вывести все контакты \n ')
+print('2 - поиск контакта\n ')
+print('3 - добавить контакт\n ')
+print('4 - изменить данные контакта\n ')
+print('5 - удалить контакт\n ')
+print('6 - выход\n ')
+
+file_path = 'phone_book.txt'
+
+def addition ():
+    with open(file_path, 'a', encoding='utf8') as open_book:
+        add_n1 = (input('Введите фамилию: ' ).title())
+        add_n2 = (input('Введите Имя: ' ).title())
+        add_n3 = (input('Введите телефон: ' ).title())
+        new_line = add_n1 +' '+add_n2 +' '+ add_n3 
+        open_book.writelines(f'\n{new_line}')
+        print(new_line)
+
+def search():
+    with open(file_path, 'r', encoding='utf8') as open_book:
+        seach_param = (input('Введите параметр для поиска: ' ).title())
+        for line in open_book:
+            if seach_param in line:
+                print(line)
+
+def delet(l):
+    delet_param = input('Введите контакт удаления: ' )
+    with open (file_path, 'w', encoding='utf8') as open_book:
+        for line in l:
+            if delet_param not in line:
+                open_book.writelines(line)
+
+def edit(l):
+    seach_param = input('Введите параметр для поиска: ' )
+    with open (file_path, 'w', encoding='utf8') as open_book:
+        for line in l:
+            if seach_param in line:
+                print(line)
+                line = line.replace((input('Контакт который хотите поменять: ')), (input('Новые данные контакта: ')))
+            open_book.writelines(line)
+
+def read():
+    with open(file_path, 'r', encoding='utf8') as open_book:
+        for line in open_book:
+            print()
+            print(line)      
+
+def tasks(task):
+   if task > 6: print('Вы ошиблись')
+   if task == 6: print('До свидания!')
+   else:
+    match task:
+        case 1: #    вывести все контакты
+            read()   
+            tasks(int(input('введите номер залачи от 1 до 6: ')))       
+
+        case 2: # поиск контактов
+            search()
+            tasks(int(input('введите номер залачи от 1 до 6: ')))
+
+        case 3: # добавить контакт
+            addition ()
+            tasks(int(input('введите номер залачи от 1 до 6: ')))
+
+        case 4: # изменить контакт
+            edit(read())
+            tasks(int(input('введите номер залачи от 1 до 6: ')))
+
+        case 5: # удалить контакт
+            delet(read())
+            tasks(int(input('введите номер залачи от 1 до 6: ')))
+            
+        case _:
+            print('введите значение из меню: ')
+            tasks(int(input('введите номер залачи от 1 до 6: ')))
+tasks(int(input('введите номер залачи от 1 до 6: ')))
+
+
